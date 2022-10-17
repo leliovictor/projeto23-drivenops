430c58a47339a307d88cc5d7bad1af90110d7ee1
leliovictor <leliov.vieira@gmail.com>
2022-10-17 02:22:56 -0300

    test: trying auto deploy on aws

--- back-end/tests/integration/app.test.ts
+++ back-end/tests/integration/app.test.ts
@@ -10,8 +10,6 @@ describe('integration test', () => {
   });
 
   it('should save a student', async () => {
-    console.log("I'm here");
-
     const students = { students: [{ name: 'juvelina' }] };
     const { status } = await agent.post('/students').send(students);
     expect(status).toBe(201);
